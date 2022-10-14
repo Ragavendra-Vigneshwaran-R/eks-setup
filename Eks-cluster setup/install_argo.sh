cf441b10bd68d64bba2ad11f6b8b8dc53f743d19
Ragavendra Vigneshwaran R <88039605+Ragavendra-Vigneshwaran-R@users.noreply.github.com>
2022-10-14 23:02:50 +0530

    Delete install_argo.sh

--- Eks-cluster setup/install_argo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-echo "Change rules in security group according to your expose port"
