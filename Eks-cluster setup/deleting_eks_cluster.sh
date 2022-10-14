917c1bebfe8583f939b82888374fd0ed162bad7d
Ragavendra Vigneshwaran R <88039605+Ragavendra-Vigneshwaran-R@users.noreply.github.com>
2022-10-15 01:33:19 +0530

    Create deleting_eks_cluster.sh

---
+++ Eks-cluster setup/deleting_eks_cluster.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Before deleting the eks cluster you have delete all services with loadbalancer and then you can delete your cluster.You can delete your services with loadbalancer using below two commands : "
+echo "kubectl get svc --all-namespaces"
+echo "kubectl delete svc <service-name>"
+echo "Did you delete all service with loadBalancer?(Y/N)"
+read answer
+if [ $ans == "Y" ]
+then 
+    echo "Enter your cluster name : "
+    read cluster_name
+    echo "Enter your region code of your cluster : "
+    read region_code
+    eksctl delete cluster --name $cluster_name --region $region_code
+else
+    echo "Kindly delete the services with loadBalacer"
+fi
