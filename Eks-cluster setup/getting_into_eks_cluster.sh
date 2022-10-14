657ed67f8c131a9b29782997c5069b0ee0ab48b8
Ragavendra Vigneshwaran R <88039605+Ragavendra-Vigneshwaran-R@users.noreply.github.com>
2022-10-14 23:15:55 +0530

    Create getting_into_eks_cluster.sh

---
+++ Eks-cluster setup/getting_into_eks_cluster.sh
@@ -0,0 +1,6 @@
+#!\bin\sh
+echo "Enter the region code : "
+read region_code
+echo "Enter the cluster name : "
+read cluster_name
+aws eks update-kubeconfig --region $region_code --name $cluster_name
